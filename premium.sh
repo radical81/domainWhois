56ce3bda8337faf2a32317ae4a98bfc039160c1a
RadX <ralobba@gmail.com>
2016-02-27 23:26:47 +0800

    Remove Chinese names, add TLA .CO

--- premium.sh
+++ premium.sh
@@ -1,25 +1,19 @@
 cd /opt/lampp/htdocs/domainWhois
-python domainwhois.py -t com -l chinese_names.txt -o chinese_names_com.txt
-if [ -s chinese_names_com.txt ]; then cat chinese_names_com.txt | mail -s "CHINESENAMES.COM" "ralobba@gmail.com"; fi
-rm chinese_names_com.txt
-python domainwhois.py -t com -l female_chinese_names.txt -o female_chinese_names_com.txt
-if [ -s female_chinese_names_com.txt ]; then cat female_chinese_names_com.txt | mail -s "FEMALECHINESENAMES.COM" "ralobba@gmail.com"; fi
-rm female_chinese_names_com.txt
 python domainwhois.py -t com -l numList.txt -o num_com.txt
 if [ -s num_com.txt ]; then cat num_com.txt | mail -s "NNNN.COM" "ralobba@gmail.com"; fi
 rm num_com.txt
 python domainwhois.py -t com -l fiveletterconsonants.txt -o ccccc_com.txt
 if [ -s ccccc_com.txt ]; then cat ccccc_com.txt | mail -s "CCCCC.COM" "ralobba@gmail.com"; fi
 rm ccccc_com.txt
-python domainwhois.py -t com -l pinyin_consonants.txt -o ppppp_com.txt
-if [ -s ppppp_com.txt ]; then cat ppppp_com.txt | mail -s "PPPPP.COM" "ralobba@gmail.com"; fi
-rm ppppp_com.txt
 python domainwhois.py -t com -l five_letter.txt -o lllll_com.txt
 if [ -s lllll_com.txt ]; then cat lllll_com.txt | mail -s "LLLLL.COM" "ralobba@gmail.com"; fi
 rm lllll_com.txt
 python domainwhois.py -t com -l cvcv.txt -o cvcv_com.txt
 if [ -s cvcv_com.txt ]; then cat cvcv_com.txt | mail -s "CVCV.COM" "ralobba@gmail.com"; fi
 rm cvcv_com.txt
+python domainwhois.py -t co -l tla.txt -o lll_co.txt
+if [ -s lll_co.txt ]; then cat lll_co.txt | mail -s "LLL.CO" "ralobba@gmail.com"; fi
+rm lll_co.txt
 python domainwhois.py -t net -l cccc.txt -o cccc_net.txt
 if [ -s cccc_net.txt ]; then cat cccc_net.txt | mail -s "CCCC.NET" "ralobba@gmail.com"; fi
 rm cccc_net.txt
